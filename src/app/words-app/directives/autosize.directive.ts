483f864a8bac43f26bd00a64d52519cebfae9bea
Oleg Sidorkin <Oleg_Sidorkin@epam.com>
2017-10-16 15:17:52 +0300

    [BF] issue_1 textarea autosize jumps

--- src/app/words-app/directives/autosize.directive.ts
+++ src/app/words-app/directives/autosize.directive.ts
@@ -5,19 +5,24 @@ import { ElementRef, HostListener, Directive} from '@angular/core';
 })
 
 export class Autosize {
-  @HostListener('input',['$event.target'])
+  @HostListener('input', ['$event.target'])
 
   onInput(textArea: HTMLTextAreaElement): void {
     this.adjust();
   }
-  constructor(public element: ElementRef){
-  }
-  ngAfterContentChecked(): void{
+
+  constructor(public element: ElementRef) { }
+
+  ngAfterContentChecked(): void {
     this.adjust();
   }
-  adjust(): void{
+  
+  adjust(): void {
     this.element.nativeElement.style.overflow = 'hidden';
     this.element.nativeElement.style.height = 'auto';
-    this.element.nativeElement.style.height = this.element.nativeElement.scrollHeight + 40 + "px";
+    const thenScrollTo = this.element.nativeElement.scrollHeight + this.element.nativeElement.scrollTop;
+    this.element.nativeElement.style.height = this.element.nativeElement.scrollHeight + 40 + 'px';
+    window.scrollTo(0, thenScrollTo + 20);
+    
   }
 }
