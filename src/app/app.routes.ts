eece7e88459f61cb7f59a9d6690ad24cd4a37e1f
Oleg Sidorkin <oleg_sidorkin@epam.com>
2017-12-13 20:16:24 +0300

    about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import {Routes, RouterModule} from '@angular/router';
 import {WordsAppComponent} from './words-app/layout/words-app/words-app.component';
 import {LoginComponent} from './words-app/layout/login/login.component';
 import {SignupComponent} from './words-app/layout/signup/signup.component';
+import {AboutComponent} from "./words-app/layout/about/about.component";
 
 //todo: implement /DD.MM.YYYY route
 //todo: implement multilanguage /about page
@@ -10,6 +11,7 @@ const appRoutes: Routes = [
   {path: '', component: WordsAppComponent},
   {path: 'signin', component: LoginComponent},
   {path: 'signup', component: SignupComponent},
+  {path: 'about', component: AboutComponent},
   {path: '**', redirectTo: ''}
 ];
 
